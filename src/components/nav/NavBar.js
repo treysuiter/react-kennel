1d9cb05dc0f9a2b21cd71b18360573f665e6a641
Trey Suiter <trey.suiter@gmail.com>
2019-12-02 09:07:56 -0600

    added comments to NavBar.js

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -4,14 +4,20 @@ import './NavBar.css'
 
 class NavBar extends Component {
 
+  //Handles logout with clearUser method passed as props and routes user to home page
+
   handleLogout = () => {
     this.props.clearUser()
     this.props.history.push("/")
   }
 
+  // Routes user to search page
+
   handleSearch = () => {
     this.props.history.push("/search")
   }
+
+  // Renders NavBar
 
   render() {
 
@@ -23,16 +29,18 @@ class NavBar extends Component {
         <nav>
           <ul className="container">
             <li><Link className="nav-link" to="/">Home</Link></li>
-            <li><Link className="nav-link" to="/locations">Locations</Link></li>   
+            <li><Link className="nav-link" to="/locations">Locations</Link></li>
+            {/* Checks if user is logged in before displaying the following links by utilizing this.props.user passed by props */}
             {(this.props.user) ?
               <>
                 <li><Link className="nav-link" to="/animals">Animals</Link></li>
                 <li><Link className="nav-link" to="/employees">Employees</Link></li>
                 <li><Link className="nav-link" to="/owners">Owners</Link></li>
-                <li><input type="text" className="nav-search" placeholder="Search" onChange={this.handleSearch}/></li>
+                <li><input type="text" className="nav-search" placeholder="Search" onChange={this.handleSearch} /></li>
                 <li><Link className="nav-link" onClick={this.handleLogout}>Logout</Link ></li>
               </> : null}
-              {(!this.props.user) ? <li><Link className="nav-link" to="/login">Login</Link></li> : null}
+            {/* If this.props.user is false, this displays login link */}
+            {(!this.props.user) ? <li><Link className="nav-link" to="/login">Login</Link></li> : null}
           </ul>
         </nav>
       </header >
